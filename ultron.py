c78a171cbfe4b832a806c7f66652ecba772d8bd6
Adarsh Jaiswal <57642211+adarshtheguru@users.noreply.github.com>
2022-04-26 01:17:35 +0530

    Update ultron.py

--- ultron.py
+++ ultron.py
@@ -1,6 +1,5 @@
 import pyttsx3
 import datetime
-from pywhatkit import sendwhatmsg, sendwhatmsg_instantly
 import speech_recognition as sr 
 import wikipedia
 import webbrowser
@@ -10,12 +9,20 @@ import smtplib
 import pyjokes
 import pywhatkit as kit
 import sys
+from PyQt5 import QtWidgets,QtCore, QtGui
+from PyQt5.QtCore import QTimer,QTime, QDate, Qt 
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
+from ultronUI import Ui_ultronUI
 
 
 
 engine = pyttsx3.init('sapi5') #MIcrosoft we can take voice
 voices = engine.getProperty('voices')
-print(voices[0].id)
+#print(voices[0].id)
 engine.setProperty('voice',voices[0].id)
 webbrowser.register('chrome',None,webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
 
@@ -37,26 +44,9 @@ def wishMe():
     print("I am Ultron Sir, working on 950 RPM, speed 20 ms. I can perform several tasks for you ,Like surfing websites, Playing music, Searching on Wikipedia, Sending an Email and Telling somes jokes to lighten your mood. Please Let me Know If you want anything now")
     speak("I am Ultron Sir, working on 950 RPM, speed 20 ms. I can perform several tasks for you ,Like surfing websites, Playing music, Searching on Wikipedia, Sending an Email and Telling somes jokes to lighten your mood. Please Let me Know If you want anything now")
 
-def takeCommand():
-    #IT takes mic info from User and returns String output
 
-    r = sr.Recognizer() #micInput as r
-    with sr.Microphone() as source:
-        print("Listening....")
-        r.pause_threshold = 1
-        audio = r.listen(source)
 
-    try:
-        print("Recognising...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: , {query}\n")
 
-    except Exception as e:
-        # print(e)
-        print("Say that again Please...")
-        speak("Say that again Please...")
-        return "None"
-    return query
 
 def sendEmail(to, content):
     server =  smtplib.SMTP('smtp.gmail.com', 587)
@@ -66,95 +56,164 @@ def sendEmail(to, content):
     server.sendmail('adarshtheguru@gmail.com',to, content)
     server.close()
 
-if __name__ == "__main__":
-    wishMe()
-    while True:
-    #if 2:
-        query = takeCommand().lower()
+class MainThread(QThread):
+    def __init__(self):
+        super(MainThread,self).__init__()
 
-        #Logic for Executing tasks based on query
-        if 'wikipedia' in query:
-            speak ('Searching Wikipedia...')
-            query = query.replace("wikipedia", "")
-            results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
+    def run(self):
+        self.TaskExecution()
 
-        elif 'open youtube' in query:
-            webbrowser.get('chrome').open("youtube.com")
+    def takeCommand(self):
+    #IT takes mic info from User and returns String output
 
-        elif 'open google' in query:
-            webbrowser.get('chrome').open("google.com")
+        r = sr.Recognizer() #micInput as r
+        with sr.Microphone() as source:
+            print("Listening....")
+            r.pause_threshold = 1
+            audio = r.listen(source)
 
-        elif 'open facebook' in query:
-            webbrowser.get('chrome').open("facebook.com")
+        try:
+            print("Recognising...")
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: , {query}\n")
 
-        elif 'open github' in query:
-            webbrowser.get('chrome').open("github.com")
+        except Exception as e:
+            # print(e)
+            print("Say that again Please...")
+            speak("Say that again Please...")
+            return "None"
+        return query
 
-        elif 'some joke' in query:
-            getJoke= pyjokes.get_joke()
-            speak(getJoke)
+    def TaskExecution(self):
+        wishMe() 
 
-        elif 'play music' in query:
-            music_dir ="E:\music"
-            songs = os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, songs[0]))
+        #if __name__ == "__main__":
+        #wishMe()
+        while True:
+        #if 2:
+            self.query = self.takeCommand().lower()
 
-        elif 'random music' in query:
-            music_dir ="E:\music"
-            songs = os.listdir(music_dir)
-            os.startfile(os.path.join(music_dir, random.choice(songs)))
+            #Logic for Executing tasks based on query
+            if 'wikipedia' in self.query:
+                speak ('Searching Wikipedia...')
+                self.query = self.query.replace("wikipedia", "")
+                results = wikipedia.summary(self.query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
 
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime('%I:%M %p')
-            speak(f"Sir, The time is {strTime}")
+            elif 'open youtube' in self.query:
+                webbrowser.get('chrome').open("youtube.com")
 
-        elif "fine" in query:
-            speak("I am great, How about you Sir, Can I help you in something")
+            elif 'open google' in self.query:
+                webbrowser.get('chrome').open("google.com")
 
-        elif 'send email' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "adarshji1999@gmail.com"
-                sendEmail(to, content)
-                print("Email has been sent")
-                speak("Email has been sent!")
+            elif 'open facebook' in self.query:
+                webbrowser.get('chrome').open("facebook.com")
 
-            except Exception as e:
-                print(e)
-                speak("Sorry Sir, I am not able to send this email")
-        
-        elif 'send mail on college ID' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "adj@sicsr.ac.in"
-                sendEmail(to, content)
-                print("Email has been sent")
-                speak("Email has been sent!")
+            elif 'open github' in self.query:
+                webbrowser.get('chrome').open("github.com")
 
-            except Exception as e:
-                print(e)
-                speak("Sorry Sir, I am not able to send this email")
+            elif 'some joke' in self.query:
+                getJoke= pyjokes.get_joke()
+                speak(getJoke)
 
-        elif 'send whatsapp' in query:
-            speak("What's the message for whatsapp, Sir")
-            cm=takeCommand()
-            kit.sendwhatmsg_instantly('+919936350601',cm)
+            elif 'play music' in self.query:
+                music_dir ="E:\music"
+                songs = os.listdir(music_dir)
+                os.startfile(os.path.join(music_dir, songs[0]))
 
-        elif 'play video on youtube' in query:
-            speak("What you want to play, Sir")
-            cm=takeCommand()
-            kit.playonyt(cm)
+            elif 'random music' in self.query:
+                music_dir ="E:\music"
+                songs = os.listdir(music_dir)
+                os.startfile(os.path.join(music_dir, random.choice(songs)))
 
-        elif "no thanks" in query:
-            speak("I am very grateful to help you Sir today, Signing off yours Ultron- The Desktop Assistant")
-            sys.exit()
+            elif 'the time' in self.query:
+                strTime = datetime.datetime.now().strftime('%I:%M %p')
+                speak(f"Sir, The time is {strTime}")
 
-        speak("Sir, Do you have any other thing for me?")
+            elif "fine" in self.query:
+                speak("I am great, How about you Sir, Can I help you in something")
+
+            elif 'send email' in self.query:
+                try:
+                    speak("What should I say?")
+                    content = self.takeCommand()
+                    to = "adarshji1999@gmail.com"
+                    sendEmail(to, content)
+                    print("Email has been sent")
+                    speak("Email has been sent!")
+
+                except Exception as e:
+                    print(e)
+                    speak("Sorry Sir, I am not able to send this email")
+            
+            elif 'send mail on college ID' in self.query:
+                try:
+                    speak("What should I say?")
+                    content = self.takeCommand()
+                    to = "adj@sicsr.ac.in"
+                    sendEmail(to, content)
+                    print("Email has been sent")
+                    speak("Email has been sent!")
+
+                except Exception as e:
+                    print(e)
+                    speak("Sorry Sir, I am not able to send this email")
+
+            elif 'send whatsapp' in self.query:
+                speak("What's the message for whatsapp, Sir")
+                cm=self.takeCommand()
+                kit.sendwhatmsg_instantly('+919936350601',cm)
+
+            elif 'play video on youtube' in self.query:
+                speak("What you want to play, Sir")
+                cm=self.takeCommand()
+                kit.playonyt(cm)
+
+            elif "no thanks" in self.query:
+                speak("I am very grateful to help you Sir today, Signing off yours Ultron- The Desktop Assistant")
+                sys.exit()
+
+            speak("Sir, Do you have any other thing for me?")
+
+startExecution = MainThread()
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_ultronUI()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.startTask)
+        self.ui.pushButton_2.clicked.connect(self.close)
+
+    def __del__(self):
+        sys.stdout = sys.__stdout__
+
+    # def run(self):
+    #     self.TaskExection
+    def startTask(self):
+        self.ui.movie = QtGui.QMovie("../adarsh/7LP8.gif")
+        self.ui.label.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        self.ui.movie = QtGui.QMovie("../adarsh/T8bahf.gif")
+        self.ui.label_2.setMovie(self.ui.movie)
+        self.ui.movie.start()
+        timer = QTimer(self)
+        timer.timeout.connect(self.showTime)
+        timer.start(1000)
+        startExecution.start()
+
+    def showTime(self):
+        current_time = QTime.currentTime()
+        current_date = QDate.currentDate()
+        label_time = current_time.toString('hh:mm:ss')
+        label_date = current_date.toString(Qt.ISODate)
+        self.ui.textBrowser.setText(label_date)
+        self.ui.textBrowser_2.setText(label_time)
 
 
-
+app = QApplication(sys.argv)
+ultron = Main()
+ultron.show()
+exit(app.exec_())
